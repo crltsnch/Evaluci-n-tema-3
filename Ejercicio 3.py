77e3dffae6252159b225438d5fe29709e2e7f661
crltsnch <crltsnch@myuax.com>
2022-10-28 17:23:47 +0200

    Ej 5. lista de listas naves

---
+++ Ejercicio 3.py
@@ -0,0 +1,4 @@
+listaNaves= [["Halcon Milenario", "Ala-X/X-Wing", "Destructor Estelar", "Lanzadera Imperial", "Supremacy", "Nave Real Imperial Tipo J"]
+    [100, 76, 55, 110, 93, 66], 
+    [22, 12, 6, 9, 15, 10], 
+    [5, 6, 0, 3, 8, 0]]
