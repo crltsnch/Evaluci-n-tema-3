8f4f81016004333227f63675baec3c1392c89f8f
crltsnch <crltsnch@myuax.com>
2022-10-28 15:45:30 +0200

    Ej 2.  calcular una matriz

--- Ejercicio 2 iterativo.py
+++ Ejercicio 2 iterativo.py
@@ -1,8 +1,12 @@
 filas = 3
-columas = 3
+columnas = 3
 matriz = []
 elementos = 0
 for i in range(filas):
-    filas()
-for j in range(columnas):
-    
+    fila=[]
+    for j in range(columnas):
+        fila.append(elementos)
+        elementos +=1
+    matriz.append(fila)
+
+
