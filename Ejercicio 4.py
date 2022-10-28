41065f69da26ed6a72b28ef81265f1faeae4fc9c
crltsnch <crltsnch@myuax.com>
2022-10-28 17:01:15 +0200

    Ej 4. funcion restar

--- Ejercicio 4.py
+++ Ejercicio 4.py
@@ -1,5 +1,16 @@
-class polinomio:
-    def __init__ (self, coeficiente=[]):
-        self.coeficientes = coeficientes[:]
+from re import X
+from tkinter import Y
+import sympy
+p1=input("Dime un polinomio: ")
+p2=input("Dime otro polinomio: ")
+print("n")
+sympy.init_printing()
+x=sympy.simbols('x')
+y=sympy.simbols('y')
+poli1=sympy.Poly(p1)
+poli2=sympy.Poly(p2)
+
+def res(p1, p2):
+    return p1-p2
 
 
