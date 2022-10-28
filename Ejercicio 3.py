f157539c4b3cd9c1d5efa2a7b1c6688b4187484b
crltsnch <crltsnch@myuax.com>
2022-10-28 17:31:41 +0200

    ordenar los nombres por longitud

--- Ejercicio 3.py
+++ Ejercicio 3.py
@@ -1,4 +1,12 @@
-listaNaves= [["Halcon Milenario", "Ala-X/X-Wing", "Destructor Estelar", "Lanzadera Imperial", "Supremacy", "Nave Real Imperial Tipo J"]
-    [100, 76, 55, 110, 93, 66], 
-    [22, 12, 6, 9, 15, 10], 
-    [5, 6, 0, 3, 8, 0]]
+listaNombre= ["Halcon Milenario", "Ala-X/X-Wing", "Destructor Estelar", "Lanzadera Imperial", "Supremacy", "Nave Real Imperial Tipo J"]
+listaLongitud=[100, 76, 55, 110, 93, 66]
+listaTripulantes= [22, 12, 6, 9, 15, 10] 
+listaPasajeros=[25, 15, 10, 10, 15, 10]
+
+import numpy
+listaNombre=numpy.array(listaNombre)
+listaLongitud=numpy.array(listaLongitud)
+inds = listaLongitud.argsort()
+sorted_listaNombre=listaNombre[listaLongitud]
+print(sorted_listaNombre)
+
